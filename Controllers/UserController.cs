eb2fe646294cac978891a37762153d34fd7cbee9
Hao Chen Liu <haochenliu@Haos-Laptop-4.local>
2024-09-02 17:14:42 +0200

    add get users and user endpoint

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -19,14 +19,34 @@ public class UserController : ControllerBase
     return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
   }
 
-  [HttpGet("test/{testValue}")]
-  public string[] Test(string testValue)
+  [HttpGet("GetUsers")]
+  public IEnumerable<User> GetUsers()
   {
-    string[] responseArray = new string[] { 
-      "test1",
-      "test2",
-      testValue
-    };
-    return responseArray;
+    string sql = @"
+          SELECT [UserId],
+            [FirstName],
+            [LastName],
+            [Email],
+            [Gender],
+            [Active] 
+          FROM TutorialAppSchema.Users";
+    IEnumerable<User> users = _dapper.LoadData<User>(sql);
+    return users;
+  }
+
+  [HttpGet("GetSingleUser/{userId}")]
+  public User GetSingleUser(int userId)
+  {
+    string sql = @"
+          SELECT [UserId],
+            [FirstName],
+            [LastName],
+            [Email],
+            [Gender],
+            [Active] 
+          FROM TutorialAppSchema.Users
+          WHERE [UserId] = " + userId.ToString();
+    User user = _dapper.LoadDataSingle<User>(sql);
+    return user;
   }
 }
--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01c2f07a6ed348dbc0a20537e249cca5560262f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ff7f351e72820baf4cb791675926dda35bf595a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
