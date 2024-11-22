a3bd3df02d6822f520ccf2dbbbb6ca0eae23b574
Hao Chen Liu <haochenliu@Haos-Laptop-4.local>
2024-11-22 17:18:22 +0100

    add posts data model

--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bf00927e0b9fe1b7fb9d9b6e7eb0e4a46d2694b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e85d8e49bab1598987dfaeb54dfe8aeef801d29")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
